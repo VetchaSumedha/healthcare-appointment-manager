8a24fcd23788b334510567b50f097d0b36bf7f92
VetchaSumedha <vetchasumedha@gmail.com>
2026-08-23 17:40:33 +0530

    Point frontend to deployed backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://healthcare-appointment-manager-8yya.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
